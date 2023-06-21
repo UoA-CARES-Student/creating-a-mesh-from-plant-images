a1190dd35e3f83c5beab050eee1b232cab1b7e2a
Eva Sorensen <eva.sorensen@halter.co.nz>
2023-06-21 17:12:00 +1200

    first attempt at combinding point clouds

--- model_processing/model_processing/create_complete_model.py
+++ model_processing/model_processing/create_complete_model.py
@@ -1,14 +1,20 @@
 import numpy as np
 import open3d as o3d
+import glob
+import os
 
+print("attempt 1")
+absolute_path = os.path.abspath("./input")
+ply_point_cloud_paths = glob.glob(absolute_path + "/*.ply")
 
-print("Load a ply point cloud, print it, and render it")
-ply_point_cloud = o3d.data.PLYPointCloud()
-pcd = o3d.io.read_point_cloud(ply_point_cloud.path)
-print(pcd)
-print(np.asarray(pcd.points))
+complete_point_cloud = o3d.geometry.PointCloud()
+
+for ply_point_cloud_path in ply_point_cloud_paths:
+    point_cloud_section = o3d.io.read_point_cloud(ply_point_cloud_path)
+    complete_point_cloud += point_cloud_section
+
 o3d.visualization.draw_geometries(
-    [pcd],
+    [complete_point_cloud],
     zoom=0.3412,
     front=[0.4257, -0.2125, -0.8795],
     lookat=[2.6172, 2.0475, 1.532],
