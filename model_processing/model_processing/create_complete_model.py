ba9ef3e6717e5b4d23fe9f7f472dabb43bbf9320
maxnankivell <maxnankivell1@gmail.com>
2023-06-21 17:45:52 +1200

    Combind point clouds then save

--- model_processing/model_processing/create_complete_model.py
+++ model_processing/model_processing/create_complete_model.py
@@ -1,11 +1,10 @@
-import numpy as np
 import open3d as o3d
 import glob
 import os
 
-print("attempt 1")
-absolute_path = os.path.abspath("./input")
-ply_point_cloud_paths = glob.glob(absolute_path + "/*.ply")
+print("Starting point cloud combination")
+absolute_path = os.path.abspath(".")
+ply_point_cloud_paths = glob.glob(absolute_path + "/input/*.ply")
 
 complete_point_cloud = o3d.geometry.PointCloud()
 
@@ -13,10 +12,13 @@ for ply_point_cloud_path in ply_point_cloud_paths:
     point_cloud_section = o3d.io.read_point_cloud(ply_point_cloud_path)
     complete_point_cloud += point_cloud_section
 
-o3d.visualization.draw_geometries(
-    [complete_point_cloud],
-    zoom=0.3412,
-    front=[0.4257, -0.2125, -0.8795],
-    lookat=[2.6172, 2.0475, 1.532],
-    up=[-0.0694, -0.9768, 0.2024],
+# o3d.visualization.draw_geometries([complete_point_cloud])
+
+o3d.io.write_point_cloud(
+    absolute_path + "/output/output.ply",
+    complete_point_cloud,
+    write_ascii=True,
+    print_progress=True,
 )
+
+print("Done! The output.ply file has been written to the output folder")
